844092ae16b13efedc4ba0dfebc6429b3ca9ac0f
alonavns <alona.vns@outlook.com>
2019-02-12 19:00:02 +0200

    contact form

--- src/components/_Common/Input/index.jsx
+++ src/components/_Common/Input/index.jsx
@@ -3,24 +3,27 @@ import React, { Component } from 'react'
 class Input extends Component {
   render() {
     return (
-      <div className={`Input ${this.props.green ? 'White' : ''}`}>
-        {this.props.textarea ? (
-          <textarea
-            style={{ ...this.props.style }}
-            placeholder={this.props.placeholder || ''}
-            onChange={this.props.onChange || null}
-          >
-            {this.props.value}
-          </textarea>
-        ) : (
-          <input
-            style={{ ...this.props.style }}
-            placeholder={this.props.placeholder || ''}
-            onChange={this.props.onChange || null}
-            value={this.props.value}
-          />
-        )}
-        {this.props.required && <i className='fa fa-asterisk' />}
+      <div className={`Input ${this.props.white ? 'White' : ''}`}>
+        {this.props.label && <div className="Label">{this.props.label}</div>}
+        <div>
+          {this.props.textarea ? (
+            <textarea
+              style={{ ...this.props.style }}
+              placeholder={this.props.placeholder || ''}
+              onChange={this.props.onChange || null}
+            >
+              {this.props.value}
+            </textarea>
+          ) : (
+              <input
+                style={{ ...this.props.style }}
+                placeholder={this.props.placeholder || ''}
+                onChange={this.props.onChange || null}
+                value={this.props.value}
+              />
+            )}
+          {this.props.required && <i className='fa fa-asterisk' />}
+        </div>
       </div>
     )
   }
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import { Button, Input } from 'components/_Common'
 import back from 'assets/images/back_contact.svg'
+import waves from 'assets/images/waves.svg'
 
 class Contact extends Component {
   render() {
@@ -30,6 +31,24 @@ class Contact extends Component {
           </div>
         </div>
         <div className="ContactForm">
+          <img src={waves} />
+          <div className="Form">
+            <div className="InputGroup">
+              <Input required placeholder='Your First Name' white label="First Name:" />
+              <Input required placeholder='Your Last Name' white label="Last Name:" />
+            </div>
+            <div className="InputGroup">
+              <Input required placeholder='Type Email' white label="Email Address:" />
+              <Input placeholder='0000 000 000 000' white label="Phone Number:" />
+            </div>
+            <Input textarea placeholder='Your Message' white label="Message Text" />
+            <div className="FormFooter">
+              <p>I agree to accept Danal Terms and Conditions for this service.</p>
+            </div>
+          </div>
+          <div className="Buttons">
+            <Button label='Submit Now' />
+          </div>
         </div>
       </div>
     )
