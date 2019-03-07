5e41064b4e9c681918234674169e6af7bfd516b1
alonavns <alona.vns@outlook.com>
2019-03-07 08:14:40 +0200

    yarn update and fix interval issue

--- src/components/Home/Testimoials/index.jsx
+++ src/components/Home/Testimoials/index.jsx
@@ -1,82 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import elementResizeEvent from 'element-resize-event'
 
-import { Testimoial } from 'components/Home'
-import photo from 'assets/images/team/member-1.png'
+import { Testimoial } from "components/Home";
+import photo from "assets/images/team/member-1.png";
 
 class Testimoials extends Component {
   state = {
     diff: 0,
     moveLock: false,
-  }
+    timer: null
+  };
   componentDidMount() {
-    var wrapperElement = document.getElementById('testimoials-wrapper');
-    var containerElement = document.getElementById('testimoials-container');
+    var wrapperElement = document.getElementById("testimoials-wrapper");
+    var containerElement = document.getElementById("testimoials-container");
     var self = this;
-    setInterval(function() {
+    const timer = setInterval(function() {
       if (self.state.moveLock) return;
       var diff = self.state.diff;
       var testimoialsCnt = 3;
       diff += 1;
-      if (diff == testimoialsCnt)
-        diff = 0;
-      var left = - diff * 440;
+      if (diff == testimoialsCnt) diff = 0;
+      var left = -diff * 440;
       // var left = - diff * 440 + wrapperElement.offsetWidth / 2 - 200;
       // if (left + 440 * testimoialsCnt <= wrapperElement.offsetWidth) {
       //   diff = -1;
       //   left = wrapperElement.offsetWidth - 440 * testimoialsCnt;
       // }
       self.setState({ diff });
-      containerElement.style.left = left + 'px';
+      containerElement.style.left = left + "px";
     }, 3000);
+    this.setState({ timer });
   }
-  handleOver = (e) => {
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+  handleOver = e => {
     this.setState({ moveLock: true });
-  }
-  handleOut = (e) => {
+  };
+  handleOut = e => {
     this.setState({ moveLock: false });
-  }
+  };
   render() {
     var testimoials = [
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Designer' },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Designer" }
       },
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Manager' },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Manager" }
       },
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Web Developer' },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Web Developer" }
       },
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Designer' },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Designer" }
       },
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Manager' },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Manager" }
       },
       {
-        description: 'Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.',
-        person: { name: 'Romain Briaux', photo: photo, role: 'Web Developer' },
-      },
+        description:
+          "Ex esse labores gloriatur his, te vel latine posidonium. Usu ei habeo inciderint, cu mei veri assentior reprimique. Vis stet veritus ut, vis hinc adolescens in, essent laoreet ei est.",
+        person: { name: "Romain Briaux", photo: photo, role: "Web Developer" }
+      }
     ];
     return (
       <div
         className={`Testimoials`}
-        id={'testimoials-wrapper'}
+        id={"testimoials-wrapper"}
         style={{ ...this.props.style }}
       >
-        <div id={'testimoials-container'} onMouseOver={this.handleOver} onMouseOut={this.handleOut}>
-        {testimoials.map((testimoial, index) => (
-          <Testimoial key={index} data={testimoial} />
-        ))}
+        <div
+          id={"testimoials-container"}
+          onMouseOver={this.handleOver}
+          onMouseOut={this.handleOut}
+        >
+          {testimoials.map((testimoial, index) => (
+            <Testimoial key={index} data={testimoial} />
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
