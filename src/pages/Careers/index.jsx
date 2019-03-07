7b73d4243003f73e9f3c835b6c9ce30a54368cd3
alonavns <alona.vns@outlook.com>
2019-03-07 02:16:19 +0200

    fixed #14

--- src/pages/Careers/index.jsx
+++ src/pages/Careers/index.jsx
@@ -107,7 +107,7 @@ class Careers extends Component {
               </Col>
               <Col lg={6} md={12}>
                 <div className="form-title">
-                  <div className="form-title-text">Benefits</div>
+                  <div className="form-title-text">Benefits:</div>
                 </div>
                 <div className="benefits-panel">
                   <div className="benefits-item">Medical Insurance</div>
